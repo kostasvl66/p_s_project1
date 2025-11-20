f2d2efda38a6d346a4b83dccc5afed34b353b2d3
Kostas <kostasvl66@gmail.com>
2025-11-20 21:34:26 +0200

    Implement argument support in main file

--- 1.4/main.c
+++ 1.4/main.c
@@ -1,20 +1,31 @@
+#include <math.h>
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 long thread_count;
+int transactions_count;
 
-void *foo(void *arg) {
-    printf("Foo works\n");
+void *write(void *arg) {
+    printf("write works\n");
     return NULL;
 }
 
+void *read(void *arg) {
+    printf("read works\n");
+    return NULL;
+}
+
+// TODO: Implement functionality for different types of locks
 int main(int argc, char *argv[]) {
-    thread_count = atol(argv[1]);
-    pthread_t *thread_handle = malloc(thread_count * sizeof(pthread_t));
+    int balance_count = atoi(argv[1]);
+    transactions_count = atoi(argv[2]);
+    long read_percentage = atol(argv[3]);
+    char *lock_type = argv[4];
+    thread_count = atol(argv[5]);
 
     // Initializing list with 5 balance accounts
-    int *balance_list = malloc(5 * sizeof(int));
+    int *balance_list = malloc(balance_count * sizeof(int));
 
     // Adding random values to list
     printf("List should be: \n");
@@ -25,12 +36,33 @@ int main(int argc, char *argv[]) {
     }
     printf("\t]\n");
 
-    for (long thread = 0; thread < thread_count; thread++) {
-        pthread_create(&thread_handle[thread], NULL, foo, (void *)&balance_list);
+    // Calculating number of reading threads based on the given percentage of reading transactions
+    double reader_calc = ((read_percentage / (float)100) * thread_count);
+    long reader_count = lround(reader_calc);
+
+    long writer_count = thread_count - reader_count;
+
+    pthread_t *writer_handle = malloc(writer_count * sizeof(pthread_t));
+    pthread_t *reader_handle = malloc(reader_count * sizeof(pthread_t));
+
+    // Initializing writer threads
+    for (long thread = 0; thread < writer_count; thread++) {
+        pthread_create(&writer_handle[thread], NULL, write, (void *)&balance_list);
     }
 
-    for (long thread = 0; thread < thread_count; thread++) {
-        pthread_join(thread_handle[thread], NULL);
+    // Initializing reader threads
+    for (long thread = 0; thread < reader_count; thread++) {
+        pthread_create(&reader_handle[thread], NULL, read, (void *)&balance_list);
+    }
+
+    // Joining writer threads
+    for (long thread = 0; thread < writer_count; thread++) {
+        pthread_join(writer_handle[thread], NULL);
+    }
+
+    // Joining reader threads
+    for (long thread = 0; thread < reader_count; thread++) {
+        pthread_join(reader_handle[thread], NULL);
     }
 
     return 0;
