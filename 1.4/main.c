a38c85c2f0cf920a66d9c2ee41dd19ee42c7549a
Kostas <kostasvl66@gmail.com>
2025-11-20 18:06:14 +0200

    Add basic thread initialization/destruction

--- 1.4/main.c
+++ 1.4/main.c
@@ -1,10 +1,22 @@
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+long thread_count;
+
+void *foo(void *arg) {
+    printf("Foo works\n");
+    return NULL;
+}
+
+int main(int argc, char *argv[]) {
+    thread_count = atol(argv[1]);
+    pthread_t *thread_handle = malloc(thread_count * sizeof(pthread_t));
+
     // Initializing list with 5 balance accounts
     int *balance_list = malloc(5 * sizeof(int));
 
+    // Adding random values to list
     printf("List should be: \n");
     printf("[");
     for (int i = 0; i < 5; i++) {
@@ -12,6 +24,14 @@ int main() {
         printf("\t%d,", balance_list[i]);
     }
     printf("\t]\n");
+
+    for (long thread = 0; thread < thread_count; thread++) {
+        pthread_create(&thread_handle[thread], NULL, foo, (void *)&balance_list);
+    }
+
+    for (long thread = 0; thread < thread_count; thread++) {
+        pthread_join(thread_handle[thread], NULL);
+    }
 
     return 0;
 }
