8e8302e7bb6dffe3950f47f2ada1fc717c05b1b4
Kostas <kostasvl66@gmail.com>
2025-11-20 17:35:34 +0200

    Build initial balance list

--- 1.4/main.c
+++ 1.4/main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
+    // Initializing list with 5 balance accounts
+    int *balance_list = malloc(5 * sizeof(int));
+
+    printf("List should be: \n");
+    printf("[\t");
+    for (int i = 0; i < 5; i++) {
+        balance_list[i] = rand();
+        printf("\t%d,", balance_list[i]);
+    }
+    printf("\t]\n");
 
     return 0;
 }
