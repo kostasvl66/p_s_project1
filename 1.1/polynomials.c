739bb8b328e52d2c0c4565918bd8a1b772109723
mtozn <altgithub0x00@gmail.com>
2025-11-21 23:40:18 +0200

    added a test benchmark thingy

--- 1.1/polynomials.c
+++ 1.1/polynomials.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+// each polynomial coefficient a non-zero int within [-MAX_ABS_COEFFICIENT_VALUE, +MAX_ABS_COEFFICIENT_VALUE]
+#define MAX_ABS_COEFFICIENT_VALUE 1000
 
 long N;
 long THREAD_COUNT;
@@ -31,10 +35,35 @@ int parse_args(int argc, char *argv[])
     return 0;
 }
 
+// calculates time difference between start and end with nsec precision
+double elapsed(struct timespec start, struct timespec end)
+{
+    return (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
+}
+
 typedef struct {
     int *coef_arr; // array of coefficients, each index maps to the same power
     long degree; // the largest power
 } Polynomial;
+
+// returns a random non-zero coefficient array of length (degree + 1) or NULL if the allocation fails
+// the caller receives ownership of the allocated memory
+// srand() must be called before the function call
+int *generate_random_coef(long degree)
+{
+    int *coef_arr = malloc((degree + 1) * sizeof(int));
+    if (!coef_arr) return NULL;
+
+    for (long i = 0; i <= degree; i++)
+    {
+        // making sure each coefficient is non-zero
+        int abs_val = 1 + rand() % MAX_ABS_COEFFICIENT_VALUE; // 1 <= abs_val <= MAX_ABS_COEFFICIENT_VALUE
+        int sign = (rand() % 2) ? 1 : -1; // random sign
+        coef_arr[i] = sign * abs_val; // 1 <= abs(coef_arr[i]) <= MAX_ABS_COEFFICIENT_VALUE
+    }
+
+    return coef_arr;
+}
 
 // initializes a polynomial; returns 0 on success, -1 otherwise
 // coef_arr must be heap allocated and is now owned by pol_init; that array is to be freed by pol_destroy (or pol_init)
@@ -73,6 +102,17 @@ void pol_print(Polynomial *pol)
     }
 }
 
+// returns 1 if pol1 and pol2 have the same degree and coefficients; 0 otherwise
+int pol_equals(Polynomial *pol1, Polynomial *pol2)
+{
+    if (pol1->degree != pol2->degree) return 0;
+    
+    for (long i = 0; i <= pol1->degree; i++)
+        if (pol1->coef_arr[i] != pol2->coef_arr[i])
+            return 0;
+
+    return 1;
+}
 
 // sets every term of a polynomial to zero without changing degree (useful for multiplication)
 void pol_set_zero(Polynomial *pol)
@@ -145,26 +185,29 @@ int main(int argc, char *argv[])
 {   
     if (parse_args(argc, argv) == -1) return 1;
 
-    // for now just trying the sum, will do the multiplication later
+    srand(time(NULL));
 
-    int *coef_arr1 = malloc((N + 1) * sizeof(int));
+    int *coef_arr1 = generate_random_coef(N);
     if (!coef_arr1) return 1;
-    for (int i = 0; i <= N; i++)
-        coef_arr1[i] = 5;
     Polynomial *pol1;
     if (pol_init(&pol1, coef_arr1, N) == -1) return 1;
     pol_print(pol1);
 
-    int *coef_arr2 = malloc((THREAD_COUNT + 1) * sizeof(int));
+    int *coef_arr2 = generate_random_coef(THREAD_COUNT);
     if (!coef_arr2) return 1;
-    for (int i = 0; i <= THREAD_COUNT; i++)
-        coef_arr2[i] = 3;
     Polynomial *pol2;
     if (pol_init(&pol2, coef_arr2, THREAD_COUNT) == -1) return 1;
     pol_print(pol2);
 
+    struct timespec start, end;
+
+    timespec_get(&start, TIME_UTC);
+
     Polynomial *prod = pol_multiply(pol1, pol2);
     if (!prod) return 1;
+    
+    timespec_get(&end, TIME_UTC);
+    printf("Elapsed time: %.9f sec\n", elapsed(start, end));
     pol_print(prod);
 
     pol_destroy(&pol1);
