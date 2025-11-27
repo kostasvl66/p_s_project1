cf1176553719d68f6af9f7cf8368d6877d4d8923
mtozn <altgithub0x00@gmail.com>
2025-11-27 18:56:11 +0200

    q1 working I guess

--- 1.5/barrier_q1.c
+++ 1.5/barrier_q1.c
@@ -1,9 +1,12 @@
+#define _POSIX_C_SOURCE 200112L // optional, used to fix editor not recognizing pthread_barrier_* (not needed in compilation)
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <pthread.h>
 
 long N;
 long THREAD_COUNT;
+pthread_barrier_t barrier;
 
 // checks command usage; returns 0 on success, -1 otherwise
 int parse_args(int argc, char *argv[])
@@ -32,12 +35,48 @@ int parse_args(int argc, char *argv[])
     return 0;
 }
 
+double elapsed(struct timespec start, struct timespec end)
+{
+    return (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
+}
+
+void *loop_worker(void *rank)
+{
+    long my_rank = (long)rank;
+
+    for (long i = 0; i < N; i++)
+    {
+        pthread_barrier_wait(&barrier);
+    }
+    
+    return NULL;
+}
+
 int main(int argc, char *argv[])
 {
     if (parse_args(argc, argv) == -1) return 1;
 
-    
+    pthread_t *thread_handles = malloc(THREAD_COUNT * sizeof(pthread_t));
+    if (!thread_handles) return 1;
 
+    struct timespec start, end;
+    timespec_get(&start, TIME_UTC);
+
+    pthread_barrier_init(&barrier, NULL, THREAD_COUNT);
+
+    // creating threads
+    for (long thread = 0; thread < THREAD_COUNT; thread++)
+        pthread_create(&thread_handles[thread], NULL, loop_worker, (void *)thread);
+
+    // waiting for threads to finish
+    for (long thread = 0; thread < THREAD_COUNT; thread++)
+        pthread_join(thread_handles[thread], NULL);
+
+    timespec_get(&end, TIME_UTC);
+    printf("Time: %.6f sec\n", elapsed(start, end));
+
+    pthread_barrier_destroy(&barrier);
+    free(thread_handles);
     return 0;
 }
 
